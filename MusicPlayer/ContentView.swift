12756da7baeeb5395847b031b450edb0eac065c8
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-11-17 20:49:10 -0300

    Primeiro commit

--- MusicPlayer/ContentView.swift
+++ MusicPlayer/ContentView.swift
@@ -9,8 +9,138 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        PlayerView()
+    }
+}
+
+struct PlayerView: View {
+    var body: some View {
+        ZStack {
+            Color.white.ignoresSafeArea(.all, edges: .all)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Button(action: {}, label: {
+                        Image(systemName: "chevron.down")
+                            .font(.system(size: 27))
+                            .foregroundColor(.black)
+                    })
+                    
+                    Spacer()
+                    
+                    Button(action: {}, label: {
+                        Image(systemName: "ellipsis")
+                            .font(.system(size: 27))
+                            .rotationEffect(Angle(degrees: 90))
+                            .foregroundColor(.black)
+                    })
+                }
+                .padding(.top)
+                
+                Text("Bring Me The Horizon")
+                    .font(.system(size: 19))
+                    .foregroundColor(.gray)
+                    .padding(.top)
+                
+                Text("Kingslayer (feat. BABYMETAL)")
+                    .font(.system(size: 25, weight: .semibold))
+                    .padding(.top, 8)
+                
+                Image("cover")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(20)
+                
+                GeometryReader { geometry in
+                    ZStack(alignment: .leading) {
+                        Capsule()
+                            .frame(height: 8)
+                            .foregroundColor(Color(#colorLiteral(red: 0.6063727736, green: 0.6431342959, blue: 0.7079168558, alpha: 1)))
+                        
+                        Capsule()
+                            .frame(width: geometry.size.width * 0.6, height: 8)
+                            .foregroundColor(Color(#colorLiteral(red: 0.2229017019, green: 0.2814418077, blue: 0.4016884565, alpha: 1)))
+                        
+                        Circle()
+                            .frame(width: 16, height: 16)
+                            .foregroundColor(Color(#colorLiteral(red: 0.2229017019, green: 0.2814418077, blue: 0.4016884565, alpha: 1)))
+                            .offset(x: (geometry.size.width * 0.6) - 8)
+                    }
+                }
+                .padding(.top)
+                .frame(height: 32)
+                
+                HStack {
+                    Text("1:26")
+                        .font(.system(size: 15))
+                        .foregroundColor(.gray)
+                    
+                    Spacer()
+                    
+                    Text("3:40")
+                        .font(.system(size: 15))
+                        .foregroundColor(.gray)
+                }
+                
+                HStack {
+                    Button(action: {}, label: {
+                        Image(systemName: "music.note.list")
+                            .font(.system(size: 27))
+                    })
+                    
+                    Spacer()
+                    
+                    HStack(spacing: 40) {
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Image(systemName: "backward.end.fill")
+                                .font(.system(size: 32))
+                        })
+                        
+                        Button(action: {}, label: {
+                            Image(systemName: "play.fill")
+                                .font(.system(size: 40))
+                                .padding()
+                                .background(Color(#colorLiteral(red: 0.2229017019, green: 0.2814418077, blue: 0.4016884565, alpha: 1)))
+                                .foregroundColor(.white)
+                                .clipShape(Circle())
+                        })
+                        
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Image(systemName: "forward.end.fill")
+                                .font(.system(size: 32))
+                        })
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {}, label: {
+                        Image(systemName: "heart")
+                            .font(.system(size: 27))
+                    })
+                }
+                .padding(.top)
+                .foregroundColor(Color(#colorLiteral(red: 0.2229017019, green: 0.2814418077, blue: 0.4016884565, alpha: 1)))
+                
+                HStack {
+                    Button(action: {}, label: {
+                        Image(systemName: "repeat")
+                            .font(.system(size: 27))
+                    })
+                    
+                    Spacer()
+                    
+                    Button(action: {}, label: {
+                        Image(systemName: "shuffle")
+                            .font(.system(size: 27))
+                    })
+                }
+                .foregroundColor(Color(#colorLiteral(red: 0.2229017019, green: 0.2814418077, blue: 0.4016884565, alpha: 1)))
+                .padding(.top)
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
---
+++ README.md
@@ -0,0 +1,25 @@
+# Music Player
+Mais um [layout do Dribbble](https://dribbble.com/shots/14573542-Music-Player-Daily-UI) que encontrei e que achei interessante implementar com SwiftUI. Dessa vez é uma tela de player com o progresso da música, opções de play e pause e controle da posição da música arrastando o slider.
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/99463994-d3d5fa80-2915-11eb-9fed-a723456a0272.png" width="400">&nbsp;
+</p>
+
+## Vídeos
+- [Vídeo no YouTube 1/3](https://youtu.be/qXUE-ZFjj0M)
+- Vídeo no YouTube 2/3 - Breve
+- Vídeo no YouTube 3/3 - Breve
+ 
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/channel/UC7UXnCqiqsV4R9lCwKh-rwg](https://www.youtube.com/channel/UC7UXnCqiqsV4R9lCwKh-rwg)
