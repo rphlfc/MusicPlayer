9a5316c5b2cad2651e2fb378da880c62bb0308b4
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-11-17 11:03:45 -0300

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Raphael Cerqueira on 17/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
